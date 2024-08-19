274f308a14d0cf1046071efdfa87ef710e2b0fc4
wiclarks@umich.edu <wiclarks@umich.edu>
2024-08-19 12:42:49 -0400

    Refactored responsibility computation into examine2d

--- python/examine2d.py
+++ python/examine2d.py
@@ -8,6 +8,7 @@
 
 import os, time
 import numpy as np
+import copy
 
 # for corner plots
 import matplotlib.pylab as plt
@@ -29,18 +30,29 @@ methods to examine them"""
         if np.size(self.flat_samples) < 1:
             self.loadsamples()
 
-        # Populate shape attributes that we will need to access
+        # Populate the shape attributes that we will need to access
         self.nsamples = 0
         self.npars = 0
         self.countsamples()
-
+        
         # Dictionary of arguments that were passed to emcee.
         self.esargs = esargs
-        self.pguess = None
-        self.obstarg = None
-        self.parset = None
-        self.lnprior = None
-        self.lnlike = None
+
+        # Quantities we get from this dictionary
+        self.inp_pguess = None
+        self.inp_obstarg = None
+        self.inp_parset = None
+        self.inp_lnprior = None
+        self.inp_lnlike = None
+
+        # Now some things we can compute on the samples
+        self.ndata = 0.
+        self.resps_samples = np.array([])   # (nsamples, ndata) - could be large
+        self.resps_avg = np.array([]) 
+
+        # Unpack the arguments passed in
+        self.unpack_esargs()
+        self.countdata()
         
     def loadsamples(self):
 
@@ -77,23 +89,109 @@ various things
             return
 
         # Views of the objects used in the emcee run
-        if not 'args' in self.esargs:
+        if not 'args' in ekeys:
             return
 
         nargs = len(self.esargs['args'])
         
         if nargs > 0:
-            self.pguess = self.esargs['args'][0]
+            self.inp_pguess = self.esargs['args'][0]
         if nargs > 1:
-            self.obstarg = self.esargs['args'][1]
+            self.inp_obstarg = self.esargs['args'][1]
         if nargs > 2:
-            self.parset = self.esargs['args'][2]
+            self.inp_parset = self.esargs['args'][2]
         if nargs > 3:
-            self.lnprior = self.esargs['args'][3]
+            self.inp_lnprior = self.esargs['args'][3]
         if nargs > 4:
-            self.lnlike = self.esargs['args'][4]
+            self.inp_lnlike = self.esargs['args'][4]
 
+    def countdata(self):
+
+        """Gets the data dimensions from the lnlike object"""
+
+        # run this without try/except first
+        self.ndata = self.inp_lnlike.obstarg.xy.shape[0]
         
+    def computeresps(self, samplesize=-1, keepmaster=True, \
+                     ireport=1000, Verbose=True):
+
+        """Computes foreground probabilities for every sample.
+
+Inputs:
+
+        samplesize = number of samples to compute
+
+        keepmaster = store all the samples in memory? [nsamples, ndata]
+        
+        ireport = report every this many rows
+
+        Verbose = report to screen
+
+"""
+
+        # The lnlike object must be populated
+        if self.inp_lnlike is None:
+            return
+
+        # local copy of the ln(like) object that we can modify
+        # in-place
+        lnlike = copy.deepcopy(self.inp_lnlike)
+
+        # how many datapoints?
+        self.countdata()
+        if self.ndata < 1:
+            return
+        
+        # How many of these do we want to do now?
+        if samplesize < 1 or samplesize >= self.nsamples:
+            imax = self.nsamples
+        else:
+            imax = samplesize
+
+        norm = 0.
+        self.resps_avg = np.zeros(self.ndata)
+            
+        if keepmaster:
+            self.resps_master = np.zeros(( imax, self.ndata ))
+
+        t0 = time.time()
+        if Verbose:
+            print("examine2d.computeresps INFO - starting responsibility loop...")
+            
+        for isample in range(imax):
+
+            # update the parameter-set and recompute everything
+            lnlike.parset.updatepars(self.flat_samples[isample])
+            lnlike.updatelnlike(lnlike.parset)
+            lnlike.calcresps()
+
+            # increment the responsibilities and the norm
+            norm += 1.
+            self.resps_avg += lnlike.resps_fg
+            
+            # If we want to store all the responsibilities per sample
+            if keepmaster:
+                self.resps_master[isample] = lnlike.resps_fg
+
+            # report out every so often
+            if isample % ireport < 1 and isample > 0 and Verbose:
+                telapsed = time.time() - t0
+                itpersec = float(isample)/telapsed
+                tremain = 0.
+                if itpersec > 0.:
+                    tremain = float(imax)/itpersec
+                print("examine2d.computeresps INFO - iteration %i of %i after %.2e seconds: %.1f it/sec. Est %.2e sec remain" \
+                      % (isample, imax, telapsed, itpersec, tremain), end='\r')
+
+        # For reporting
+        t1 = time.time()
+        if Verbose:
+            print("")
+            print("examine2d.computeresps INFO - loops took %.2e seconds for %.2e samples" % (t1-t0, imax))
+        
+        # Evaluate the average
+        self.resps_avg /= norm
+                
 def showcorner(flat_samples=np.array([]), \
                labels=None, truths=None, \
                fignum=4, pathfig='test_corner_oo.png', \
